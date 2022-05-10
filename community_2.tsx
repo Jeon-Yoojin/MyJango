5ab67f0eb63b45254bcc98a6b1d615e9e833b382
hayoung26 <104810574+hayoung26@users.noreply.github.com>
2022-05-10 20:26:41 +0900

    Update community_2.tsx

--- community_2.tsx
+++ community_2.tsx
@@ -1,57 +1,65 @@
 
 import React from 'react';
 import {useState} from 'react';
-import {SafeAreaView, View, Text, TextInput, ScrollView} from 'react-native';
+import {SafeAreaView, View, Text, TextInput, ScrollView, Button} from 'react-native';
 import {Colors} from "react-native-paper";
 import {StyleSheet, Modal, Pressable} from "react-native";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const style = StyleSheet.create({
-  mainViewStyle: {flex: 1, backgroundColor: "red", justifyContent:
- "center"},
+ mainViewStyle: {flex: 1, backgroundColor: "red", justifyContent: "center"},
  view: {flex: 1, alignItems: 'center', backgroundColor: Colors.amber100},
  scrollview: {flex: 1, backgroundColor: Colors.indigo200},
  infoView: {borderWidth: 1, width:400},
  temp : {flexDirection: "row"},
- temp01 : {backgroundColor:Colors.red200, alignContent:"center", flexDirection: "row"},
- temp02 : {backgroundColor:Colors.indigo200, alignContent:"center", flexDirection: "row"},
- temp03 : {backgroundColor:Colors.yellow400, alignContent:"center", flexDirection: "row"},
- expireddate1 : {backgroundcolor: "green", borderWidth:1, borderRadius: 10, margin: 10, fontSize: 18, padding:3},
- expireddate : {backgroundcolor:Colors.grey400, borderWidth:1, borderRadius: 10, margin: 10, fontSize: 18, padding:3},
+ temp01 : {alignContent:"center", flexDirection: "row"},
+ temp02 : {backgroundColor:Colors.grey300, alignContent:"center", flexDirection: "row"},
+ temp03 : {alignContent:"center", flexDirection: "row"},
+ expireddate1 : {backgroundColor:Colors.green200, borderWidth:1, borderRadius: 10, margin: 10, fontSize: 18, padding:3},
+ expireddate : {backgroundColor:Colors.grey300, borderWidth:1, borderRadius: 10, margin: 10, fontSize: 18, padding:3},
 
-cameraview : { width:300, height:300},
   TitleText: {textAlign : "left", fontWeight : 'bold', margin : 20, fontSize: 25},
+  TitleTextip: {textAlign : "center", margin : 2, fontSize: 23},
   textInputStyle: {width : 210, height : 50, borderWidth: 1, backgroundColor: "white", margin:2}
  })
 
+const App = () => {
 const [modalVisible, setModalVisible] = useState(false);
-const addfr = () => {<Modal>
+  return (
+ <SafeAreaView>
+    <View style={style.temp01}>
+      <Text style = {style.TitleText} >커뮤니티</Text>
+     <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+        onShow={()=>console.log("onShow")}
+>
   <View style={style.temp}>
   <Text style = {{fontSize: 23, fontWeight: 'bold', margin: 20}}>친구 추가</Text>
   </View>
   <View style={style.temp}>
-    <Text>이름</Text>
+    <Text style={style.TitleTextip}>이름     </Text>
     <TextInput style = {style.textInputStyle}
     placeholder = "            "
     keyboardType = "default"></TextInput>
   </View>
   <View style={style.temp}>
-    <Text>이메일</Text>
+    <Text style={style.TitleTextip}>이메일 </Text>
     <TextInput style = {style.textInputStyle}
     placeholder = "            "
     keyboardType = "default"></TextInput>
   </View>
-  <Pressable
-    onPress={() => setModalVisible(!modalVisible)}
-    />
-</Modal>;};
+  <Button title="저장" onPress={() => setModalVisible(!modalVisible)}></Button>
+</Modal>
+<Pressable
+        onPress={() => setModalVisible(true)}>
+      <Text style={{fontSize : 17, color:Colors.grey400, position:"absolute", left:150, top:25}}>친구 추가</Text>
+      </Pressable>
 
-const App = () => {
- return (
- <SafeAreaView>
-    <View style={style.temp01}>
-      <Text style = {style.TitleText} >커뮤니티</Text>
-      <Text onPress={addfr} style = {{fontSize : 17, color:Colors.grey400, position:"absolute", right:12, top:25}}>친구 추가</Text>
     </View>
 
 
