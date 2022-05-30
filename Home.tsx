5dc18bbbcd3d7a0b7a5f21110950da18ec4befd3
jsh1230 <104811073+jsh1230@users.noreply.github.com>
2022-05-30 18:33:09 +0900

    Update Home.tsx

--- Home.tsx
+++ Home.tsx
@@ -1,29 +1,33 @@
 import React from "react";
-import {StyleSheet, SafeAreaView} from "react-native";
-import {TopBar, View, Text, UnderlineText} from "./theme";
+import {StyleSheet, SafeAreaView, View, Text} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import { useCallback } from "react";
 
 
 export default function Home(){
     const navigation = useNavigation();
- const goLeft =
-useCallback(()=>navigation.navigate("Ingredients_add"),[]);
+    const goLeft =
+useCallback(()=>navigation.navigate("DBtest"), []);
+
  const goRight =
 useCallback(()=>navigation.navigate("Recipe_list"), []);
+const goDBtest =
+useCallback(()=>navigation.navigate("DBtest2"), []);
 
  return (
- <SafeAreaView style = {styles.safe}>
-    <View style = {styles.view}>
-        <TopBar>
-            <UnderlineText onPress={goLeft} style = {styles.text}>Go Left</UnderlineText>
-            <UnderlineText onPress={goRight} style = {styles.text}>Go Right</UnderlineText>
-        </TopBar>
- <View style = {styles.view}>
- <UnderlineText style = {styles.text}>Mobile
-Software</UnderlineText>
+ <SafeAreaView style={styles.safe}>
+    <View style={styles.view}>
+    
+            <Text style={styles.text} onPress={goLeft}>Go Left</Text>
+            <Text onPress={goRight} style = {styles.text}>Go Right</Text>
+            <Text onPress={goDBtest} style = {styles.text}>Go DBtest</Text>
+     
+ <View>
+
  <Text style = {styles.text}>Fall, 2021</Text>
+ 
  </View>
+
  </View>
 
 
