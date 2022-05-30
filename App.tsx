75eae828c1ee90ac969aa353fff5e6c41662a84c
jsh1230 <104811073+jsh1230@users.noreply.github.com>
2022-05-30 18:31:07 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
 import React, {useCallback, useState} from "react";
-import {NavigationContainer, DefaultTheme, DarkTheme} from "@react-navigation/native";
-import {ToggleThemeProvider} from "./ToggleThemeProvider"
-import { useColorScheme } from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
 import MainNavigator from "./MainNavigator";
+import NavBar from "./NavBar";
 const App = ()=>{
- const scheme = useColorScheme();
- const [theme, setTheme] = useState(scheme == "dark" ? DarkTheme :
-DefaultTheme);
- const toggleTheme = useCallback(()=>{
- setTheme(({dark}) => {
- return dark ? DefaultTheme : DarkTheme;
- });
- }, []);
- return (<ToggleThemeProvider toggle = {toggleTheme}>
- <NavigationContainer theme = {theme}>
+ 
+ return (
+ <NavigationContainer>
  <MainNavigator/>
+ <NavBar></NavBar>
  </NavigationContainer>
- </ToggleThemeProvider>);
+ );
 }
 export default App;
+
+
