b3ddb2a64f612d496a7292f91cfb32ed557b9264
Jeon-Yoojin <jeonryu@sookmyung.ac.kr>
2022-06-02 17:00:20 +0900

    navbar 업데이트, img 폴더 생성

--- component/NavBar.js
+++ component/NavBar.js
@@ -1,40 +1,107 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity, Dimensions} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Main from '../main/Main';
+import ManagePage from './ManagePage';
+import Recipe_list from '../recipe_list/recipe_list';
+import Community from '../community/Community';
 
+//이미지 경로
 const ChatFillPath = require('../../android/app/src/main/assets/images/chat-fill.png');
 const FridgeFillPath = require('../../android/app/src/main/assets/images/fridge-fill.png');
 const HomeFillPath = require('../../android/app/src/main/assets/images/home-fill.png');
 const LightbulbFillPath = require('../../android/app/src/main/assets/images/lightbulb-fill.png');
 
+const fullWidth = Dimensions.get('window').width
+const fullHeight = Dimensions.get('window').height
+
+function MainScreen() {
+    return (
+      <View style={{ flex: 1}}>
+        <Main></Main>
+      </View>
+    );
+  }
+
+  function OtherScreen() {
+    return (
+      <View style={{ flex: 1}}>
+        <Text>OtherScreen</Text>
+      </View>
+    );
+  }
+
+  const Tab = createBottomTabNavigator();
+
+  function MyTabs() {
+    return (
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: '#FF615B',
+          headerShown:false,
+          tabBarStyle:{height:75},
+        }}
+      >
+        <Tab.Screen
+          name="홈"
+          component={Main}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Image style={styles.Img} source={HomeFillPath} tintColor={color} />
+            ),
+            tabBarLabelStyle:{
+                fontSize: 13.5
+            }
+          }}
+        />
+        <Tab.Screen
+          name="재료 관리"
+          component={ManagePage}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+                <Image style={styles.Img} source={FridgeFillPath} tintColor={color}/>
+            ),
+            tabBarLabelStyle:{
+                fontSize: 13.5
+            }
+          }}
+        />
+        <Tab.Screen
+          name="레시피 추천"
+          component={Recipe_list}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Image style={styles.Img} source={LightbulbFillPath} tintColor={color} />
+            ),
+            tabBarLabelStyle:{
+                fontSize: 13.5
+            }
+          }}
+        />
+        <Tab.Screen
+          name="커뮤니티"
+          component={Community}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Image style={styles.Img} source={ChatFillPath} tintColor={color} />
+            ),
+            tabBarLabelStyle:{
+                fontSize: 13.5
+            }
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
 
 const NavBar = (props)=>{
+
     return(
-        <View style={styles.navbar}>
-            <TouchableOpacity>
-            <View style={styles.navbarElement}>
-            <Image style={styles.Img} source={HomeFillPath} tintColor='#FF615B'/>
-            <Text style={styles.text}>홈</Text>
-            </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <View style={styles.navbarElement}>
-            <Image style={styles.Img} source={FridgeFillPath}/>
-            <Text style={styles.text}>재료 관리</Text>
-            </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <View style={styles.navbarElement}>
-            <Image style={styles.Img} source={LightbulbFillPath}/>
-            <Text style={styles.text}>레시피 추천</Text>
-            </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <View style={styles.navbarElement}>
-            <Image style={styles.Img} source={ChatFillPath}/>
-            <Text style={styles.text}>커뮤니티</Text>
-            </View>
-            </TouchableOpacity>
-        </View>
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
     )
 }
 
@@ -45,6 +112,7 @@ const styles = StyleSheet.create({
         paddingLeft: 20,
         paddingRight: 33,
         paddingTop: 15,
+        width: fullWidth,
 
         borderTopColor:'rgba(0,0,0,0.05)',
         borderTopWidth:1.5,
