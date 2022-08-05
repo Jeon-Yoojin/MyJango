1604fe64ff71a91195cd5baae5991c3dcdde46a1
jsh1230 <104811073+jsh1230@users.noreply.github.com>
2022-08-05 16:12:51 +0900

    Create images.js

---
+++ community/present_condition/images.js
@@ -0,0 +1,13 @@
+import CheckBoxOutline from './assets/check_box_outline_blank_FILL0_wght400_GRAD0_opsz48.png';
+import CheckBox from './assets/check_box_FILL0_wght400_GRAD0_opsz48.png';
+import Delete from './assets/cancel_FILL1_wght400_GRAD0_opsz48.png';
+import DeleteGrey from './assets/cancel_FILL1_wght400_GRAD0.png'
+import Edit from './assets/edit_FILL0_wght400_GRAD0_opsz48.png';
+
+export const images = {
+  uncompleted: CheckBoxOutline,
+  completed: CheckBox,
+  delete: Delete,
+  deletecompleted: DeleteGrey,
+  edit: Edit,
+};
