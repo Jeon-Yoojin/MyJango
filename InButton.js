a89d7947e791e3d47bdcdfe6adc2b8b8d9b57a33
hayoung26 <104810574+hayoung26@users.noreply.github.com>
2022-08-12 13:34:59 +0900

    Create InButton.js

---
+++ InButton.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+const InButton = (Props) => {
+
+    const styles = StyleSheet.create({
+        btnPress: {
+            backgroundColor: "skyblue", borderRadius: 10, margin: 10, fontSize: 18, padding:3
+          },
+          btnNormal: {
+            backgroundColor: "lightgray", borderRadius: 10, margin: 10, fontSize: 18, padding:3
+          }
+        });
+    const [isPress, setIsPress] = useState(false);
+    const touchProps = {
+  
+      style: isPress ? styles.btnPress : styles.btnNormal,
+      onPress: () => setIsPress(!isPress),
+  
+    };
+  
+
+  return(
+    <TouchableOpacity {...touchProps} underlayColor='skyblue'>
+        <Text style={{fontSize:18}}>{Props.text}</Text>
+        </TouchableOpacity>
+        )
+  };
+export default InButton;
