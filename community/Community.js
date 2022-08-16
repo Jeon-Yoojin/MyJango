7323a234889507193adbc5043d0bcd618385ec04
jsh1230 <104811073+jsh1230@users.noreply.github.com>
2022-08-16 15:26:11 +0900

    Update Community.js

--- community/Community.js
+++ community/Community.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
-import { StyleSheet, Modal, Pressable, TouchableOpacity, Alert , SafeAreaView, View, Text, TextInput, ScrollView, Button, TouchableHighlight } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, TouchableOpacity, Alert , SafeAreaView, View, Text, ScrollView, TouchableHighlight } from 'react-native';
 import { Colors } from "react-native-paper";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon2 from 'react-native-vector-icons/FontAwesome';
 import Mailer from 'react-native-mail';
 import ModalFriends from './ModalFriends';
 import ModalPresentCondition from './present_condition/ModalPresentCondition'
+import InButton from './InButton';
 
 
 export const sendEmailWithMailer = (
@@ -36,27 +38,54 @@ const Community = () => {
   const child1Ref = useRef();
   const child2Ref = useRef();
 
-  const [color1, setcolor1] = useState("lightgrey");
-  const [color2, setcolor2] = useState("lightgray");
-  const [color3, setcolor3] = useState("lightgray");
-  const [color4, setcolor4] = useState("lightgray");
-  const [color5, setcolor5] = useState("lightgray");
-  const [color6, setcolor6] = useState("lightgray");
-  const [color7, setcolor7] = useState("lightgray");
-  const [color8, setcolor8] = useState("lightgray");
+  const [color, setColor] = useState("");
+
+  const share1 = () => {
+    Alert.alert("공유","친구에게 재료를 공유했습니다.",
+    [{text: "확인", onPress: ()=>{console.log("share")}}]);
+    setColor("lightgray");
+  }
+
+  const delete1 = () => {
+    setColor("lightgray");``
+  }
+
+  const [myToMe, setMyToMe] = useState(["감자","당근","김","asdfa","asdfas"])
+
+inde = {
+  data: ["감자","당근","김"]
+};
+ind = {
+  data: ["돼지고기","파"]
+};
+fri = {
+  data: ["냉장고", "털이범","ㄱㄴㄷ","a","b","c","ds"]
+};
+
+
+
 
   const styles = StyleSheet.create({
-    temp : {
-      flexDirection: "row"
-    },
     temp01 : {
       alignContent:"center", flexDirection: "row"
     },
-    temp2 : {
-      backgroundColor:Colors.grey300, alignContent:"center", margin: 20, borderRadius: 10
+    myPresentCondition : {
+      borderColor:'green', borderWidth:1, backgroundColor:Colors.grey300, alignContent:"center", margin: 20, borderRadius: 10
     },
-    temp02 : {
-      backgroundColor:Colors.grey300, alignContent:"center", flexDirection: "row", borderRadius: 10
+    myText : {
+      borderColor:'red', borderWidth:1, backgroundColor:Colors.grey300, alignContent:"center", flexDirection: "row", borderRadius: 10
+    },
+    myIng : {
+      flexDirection:'row',borderWidth:1, borderColor:'red', alignContent:'space-between'
+    },  
+    myToMe : {
+      borderWidth:1, borderRadius: 10, flex:1
+    },
+    myFromMe : {
+      borderWidth:1, borderColor:'blue', borderRadius: 10, flex:1
+    },
+    modifyMine : {
+      borderWidth:1, alignItems:"flex-end"
     },
     temp3 : {
       backgroundColor:Colors.grey200, alignContent:"center", margin: 20, borderRadius: 10
@@ -64,251 +93,132 @@ const Community = () => {
     temp03 : {
       backgroundColor:Colors.grey200, alignContent:"center", flexDirection: "row", borderRadius: 10
     },
+    temp03_1 : {
+      position:"relative", left: 5, alignContent:"center", flexDirection: "row", borderRadius: 10
+    },
+    temp03_2 : {
+      position:"relative", left: 185, top: -50, alignContent:"center", flexDirection: "row", borderRadius: 10, marginBottom: -50
+    },
     expireddate0 : {
-      fontSize: 18
-    },
-    expireddate1 : {
-      backgroundColor:Colors.green200, borderRadius: 10, margin: 10, padding:3
-    },
-    expireddate01 : {
-      backgroundColor:Colors.green200, borderRadius: 10, margin: 10, padding:3, alignContent:"flex-end"
-    },
-    expireddate2_1 : {
-      backgroundColor:color1, borderRadius: 10, margin: 10, fontSize: 18, padding:3
-    },
-    expireddate2_2 : {
-      backgroundColor:color2, borderRadius: 10, margin: 10, fontSize: 18, padding:3
-    },
-    expireddate2_3 : {
-      backgroundColor:color3, borderRadius: 10, margin: 10, fontSize: 18, padding:3
-    },
-    expireddate2_4 : {
-      backgroundColor:color4, borderRadius: 10, margin: 10, fontSize: 18, padding:3
-    },
-    expireddate2_5 : {
-      backgroundColor:color5, borderRadius: 10, margin: 10, fontSize: 18, padding:3
-    },
-    expireddate2_6 : {
-      backgroundColor:color6, borderRadius: 10, margin: 10, fontSize: 18, padding:3
-    },
-    expireddate2_7 : {
-      backgroundColor:color7, borderRadius: 10, margin: 10, fontSize: 18, padding:3
-    },
-    expireddate2_8 : {
-      backgroundColor:color8, borderRadius: 10, margin: 10, fontSize: 18, padding:3
+      fontSize: 18, color: 'white'
     },
     TitleText: {
-      textAlign : "left", fontWeight : 'bold', margin : 20, fontSize: 25
-    },
-    TitleTextip: {
-      textAlign : "center", margin : 2, fontSize: 23
+      textAlign : "left", fontWeight : 'bold', margin : 20, marginRight:40, fontSize: 25
     },
     sharetext: {
-      position:"absolute", left:120, margin: 10
+      position: "relative", left:130, margin: 10
     },
-    edittext: {
-      position:"absolute", left:0, margin: 8
+    deletetext: {
+      position:"relative", left:150, margin: 10
     },
-    textInputStyle: {
-      width : 210, height : 40, borderWidth: 1, backgroundColor: "white", margin:2
+    scrollView: {
+      maxHeight:400
     }
    });
 
-  const share1 = () => {
-    Alert.alert("공유","친구에게 재료를 공유했습니다.",
-    [{text: "확인", onPress: ()=>{console.log("share")}}]);
-    setcolor1("lightgray");
-    setcolor2("lightgray");
-    setcolor3("lightgray");
-    setcolor4("lightgray");
-    setcolor5("lightgray");
-  }
-
-const share2 = () => {
-  Alert.alert("공유","친구에게 재료를 공유했습니다.",
-  [{text: "확인", onPress: ()=>{console.log("share")}}]);
-  setcolor6("lightgray");
-  setcolor7("lightgray");
-  setcolor8("lightgray");
-}
-
-const action1 = () => {
-  setcolor1("skyblue");
-};
-const action2 = () => {
-  setcolor2("skyblue");
-};
-const action3 = () => {
-  setcolor3("skyblue");
-};
-const action4 = () => {
-  setcolor4("skyblue");
-};
-const action5 = () => {
-  setcolor5("skyblue");
-};
-const action6 = () => {
-  setcolor6("skyblue");
-};
-const action7 = () => {
-  setcolor7("skyblue");
-};
-const action8 = () => {
-  setcolor8("skyblue");
-};
-
-const [to, setText_e] = useState("");
-const [in1, setText01] = useState("감자");
-const [in2, setText02] = useState("당근");
-const [in3, setText03] = useState("김");
-const [in4, setText04] = useState("돼지고기");
-const [in5, setText05] = useState("파");
-const [inde1, setText1] = useState("감자");
-const [inde2, setText2] = useState("당근");
-const [inde3, setText3] = useState("김");
-const [inde4, setText4] = useState("돼지고기");
-const [inde5, setText5] = useState("파");
-useEffect(()=>{
-  setInterval(()=>{
-  setText1;
-  });
-  setInterval(()=>{
-  setText2;
-  });
-  setInterval(()=>{
-    setText3;
-  });
-  setInterval(()=>{
-    setText4;
-  });
-  setInterval(()=>{
-    setText5;
-  });   
-  setInterval(()=>{
-    setcolor1;
-  }); 
-  setInterval(()=>{
-    setcolor2;
-  }); 
-  setInterval(()=>{
-    setcolor3;
-  });          
-  setInterval(()=>{
-    setcolor4;
-  }); 
-  setInterval(()=>{
-    setcolor5;
-  }); 
-  setInterval(()=>{
-    setcolor6;
-  }); 
-  setInterval(()=>{
-    setcolor7;
-  }); 
-  setInterval(()=>{
-    setcolor8;
-  }); 
-},);
-
-
+  
   return (
- <SafeAreaView>
+  <SafeAreaView>
     <View style={styles.temp01}>
-      <Text style = {styles.TitleText}>커뮤니티</Text>
+      <Text style = {styles.TitleText} onPress={()=>{console.log("current searchId  ",searchId)}} >커뮤니티</Text>
       <ModalFriends ref={child1Ref}></ModalFriends>
       <TouchableOpacity onPress={()=>{child1Ref.current.toggleModal();}}>
         <Text style={{fontSize : 17, color:Colors.grey400, position:"absolute", left:150, top:25}}>친구 추가</Text>
       </TouchableOpacity>
+    </View>
+
+    <View style={styles.myPresentCondition}>
+      <View style={styles.myText}>  
+        <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 15}}>부족                           </Text>
+        <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 15}}>넉넉</Text>
+      </View>
+
+      <View style={styles.myIng}>
+
+      <View style={styles.myToMe}> 
+        {myToMe.map((item,index)=>{
+          return (
+            <TouchableHighlight key={index} style={{backgroundColor: 'rgb(' +
+              Math.floor(Math.random() * 256) +
+              ',' + Math.floor(Math.random() * 256) +
+              ',' + Math.floor(Math.random() * 256) +
+              ')', borderRadius: 10, margin: 10, padding:3}}>
+            <Text style={styles.expireddate0}>{item}</Text></TouchableHighlight>
+  
+          )
+        })}
+      </View>
+
+      <View style={styles.myFromMe}>
+        {ind.data.map((item,index)=>{
+          return (
+            <TouchableHighlight key={index} style={{backgroundColor: 'rgb(' +
+              Math.floor(Math.random() * 256) +
+              ',' + Math.floor(Math.random() * 256) +
+              ',' + Math.floor(Math.random() * 256) +
+              ')', borderRadius: 10, margin: 10, padding:3}}>
+            <Text style={styles.expireddate0}>{item}</Text></TouchableHighlight>
+  
+          )
+        })} 
+      </View>
+
+      </View>
+
+      <View style={styles.modifyMine}>
+      <ModalPresentCondition ref={child2Ref} ></ModalPresentCondition>
+        <TouchableOpacity onPress={()=>{child2Ref.current.toggleModal();
+          child2Ref.current.initFromMeData(); child2Ref.current.initToMeData();}}>  
+          <Icon name="pencil" size={30} color={Colors.black} style={styles.edittext}/>
+        </TouchableOpacity>
+      </View>
 
     </View>
 
-<View style={styles.temp2}>
- <View style={styles.temp02}>  
- <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 15}}>부족                         </Text>
- <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 15}}>넉넉</Text>
-  </View>
+    <View style={styles.temp01}> 
+      <Text style = {{fontSize: 23, fontWeight: 'bold', margin: 20}}>친구</Text>
+    </View>
 
-  <View style={styles.temp02}> 
-  <TouchableHighlight style={styles.expireddate1}>
-    <Text style={styles.expireddate0}>{inde1}</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate1}>
-    <Text style={styles.expireddate0}>{inde2}</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate1}>
-    <Text style={styles.expireddate0}>{inde3}</Text></TouchableHighlight>
-  <Text>    </Text>
-  <TouchableHighlight style={styles.expireddate01}>
-    <Text style={styles.expireddate0}>{inde4}</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate01}>
-    <Text style={styles.expireddate0}>{inde5}</Text></TouchableHighlight>
-        
-  <ModalPresentCondition ref={child2Ref} ></ModalPresentCondition>
-  <TouchableOpacity onPress={()=>{child2Ref.current.toggleModal(); 
-    child2Ref.current.initToMeData();}} >  
-  <Icon name="pencil" size={30} color={Colors.black} style={styles.edittext}/>
-  </TouchableOpacity>
-</View>
-</View>
+    <ScrollView style={styles.scrollView}>
+      {fri.data.map((item, index)=>{
+        return (
+          <View style={styles.temp3} key={index}>
+            <View style={styles.temp03}>  
+              <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 20}}>부족                        </Text>
+              <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 20}}>넉넉</Text>
+            </View>
 
+            <View style={styles.temp03_1}>
+              {ind.data.map((item,index)=>{
+                return (
+                  <InButton key={index} text={item} style={{backgroundColor: color}}/>
+                )
+              })}
+            </View>
 
-  <View style={styles.temp01}> 
-    <Text style = {{fontSize: 23, fontWeight: 'bold', margin: 20}}>친구</Text>
-  </View>
-<ScrollView>
-<View style={styles.temp3}>
-  <View style={styles.temp03}>  
- <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 20}}>부족                        </Text>
- <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 20}}>넉넉</Text>
-  </View>
+            <View style={styles.temp03_2}>
+              {inde.data.map((item, index)=>{
+                return (
+                  <InButton key={index} text={item} style={{backgroundColor: color}}/>
+                )
+              })}
+            </View>
 
-  <View style={styles.temp03}>  
-  <Text>  </Text>
-  <TouchableHighlight style={styles.expireddate2_1} onPress={action1}>
-    <Text style={styles.expireddate0}>감자</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate2_2} onPress={action2}>
-    <Text style={styles.expireddate0}>당근</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate2_3} onPress={action3}>
-    <Text style={styles.expireddate0}>김</Text></TouchableHighlight>
-<Text>    </Text>
-  <TouchableHighlight style={styles.expireddate2_4} onPress={action4}>
-    <Text style={styles.expireddate0}>돼지고기</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate2_5} onPress={action5}>
-    <Text style={styles.expireddate0}>파</Text></TouchableHighlight>
-</View>
+            <View style={styles.temp03}> 
+              <Icon name="account" size={30} color={Colors.black} style={{margin: 5}}/>
+              <Text style = {{fontSize: 18, margin: 10}}>{item}</Text>
+              <TouchableOpacity onPress = {share1}> 
+                <Icon name="share" size={30} color={Colors.black} style={styles.sharetext}/>
+              </TouchableOpacity> 
+              <TouchableOpacity onPress = {delete1}> 
+                <Icon2 name="refresh" size={30} color={Colors.black} style={styles.deletetext}/>
+              </TouchableOpacity> 
+            </View>
+          </View>
+        )})}
+    </ScrollView>
 
-  <View style={styles.temp03}> 
-  <Icon name="account" size={30} color={Colors.black} style={{margin: 5}}/>
-  <Text style = {{fontSize: 18, margin: 10}}>냉장고 털이범님</Text>
-  <TouchableOpacity onPress = {share1}> 
-    <Icon name="share" size={30} color={Colors.black} style={styles.sharetext}/>
-  </TouchableOpacity> 
-  </View>
-</View>
-
-<View style={styles.temp3}>
-  <View style={styles.temp03}>  
- <Text style = {{fontSize: 18, fontWeight: 'bold', margin: 20}}>부족                        </Text>
-  </View>
-
-  <View style={styles.temp03}>  
-  <Text>  </Text>
-  <TouchableHighlight style={styles.expireddate2_6} onPress={action6}>
-    <Text style={styles.expireddate0}>감자</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate2_7} onPress={action7}>
-    <Text style={styles.expireddate0}>당근</Text></TouchableHighlight>
-  <TouchableHighlight style={styles.expireddate2_8} onPress={action8}>
-    <Text style={styles.expireddate0}>김</Text></TouchableHighlight>
-</View>
-
-  <View style={styles.temp03}> 
-  <Icon name="account" size={30} color={Colors.black} style={{margin:5}}/>
-  <Text style = {{fontSize: 18, margin: 10}}>냉장고 털이범님</Text>
-  <TouchableOpacity onPress = {share2}> 
-    <Icon name="share" size={30} color={Colors.black} style={styles.sharetext}/>  
-  </TouchableOpacity> 
-  </View>
-</View>
-</ScrollView>
-</SafeAreaView>
+</SafeAreaView> 
  );
 };
 
