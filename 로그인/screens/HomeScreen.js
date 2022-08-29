c8f13f3dc78e4e1aa417b9b37fbdd5701aef4b86
hayoung26 <104810574+hayoung26@users.noreply.github.com>
2022-08-29 21:51:09 +0900

    Update HomeScreen.js

--- 로그인/screens/HomeScreen.js
+++ 로그인/screens/HomeScreen.js
@@ -1,9 +1,22 @@
 import React from "react"
 import {View, Text} from "react-native"
+import {COLORS} from '../constants/theme';
+import { signOut, singIn } from "../utils/auth";
 
 const HomeScreen = () => {
+    const handleOnSubmit1 = () => {
+        signOut();
+    };
  return (
- <View><Text>HomeScreen</Text></View>
+    <View>
+        <Text style={{ alignItems: 'center', margin: 20, fontSize: 20}}>홈화면</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+            <Text style={{marginLeft: 4, color: COLORS.gray}}
+            onPress={handleOnSubmit1}>로그아웃</Text>
+        </View>
+
+    </View>
+            
  );
 };
 
