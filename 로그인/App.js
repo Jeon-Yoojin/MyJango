f7585e45f0cb73ccbead5792b5f419a92e2f870b
hayoung26 <104810574+hayoung26@users.noreply.github.com>
2022-08-29 21:50:48 +0900

    Update App.js

--- 로그인/App.js
+++ 로그인/App.js
@@ -1,11 +1,13 @@
 import 'react-native-gesture-handler';
-import React from "react"
+import React, {useState, useEffect} from "react"
 import {View, Text} from "react-native"
 import {NavigationContainer} from '@react-navigation/native';
 import AuthStackNavigator from './navigators/AuthStackNavigator';
 import firestore,{firebase} from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import { HomeScreen } from './screens';
 
-firebase.initializeApp({
+!firebase.apps.length ? firebase.initializeApp({
    appId: 'myjango-da9a3',
    apiKey: 'AIzaSyCN-tD1-DY8irEB7UKvn0n4g06r4shKVvI',
    projectId: '359921450232',
@@ -13,15 +15,31 @@ firebase.initializeApp({
    storageBucket: '',
    messagingSenderId: '',
    clientId: '',
- });
+ }) : firebase.app()
 
-
-const App = () => {
- return (
- <NavigationContainer>
-    <AuthStackNavigator/>
- </NavigationContainer>
- );
-};
-
-export default App;
+ const App = () => {
+   const [currentUser, setCurrentUser] = useState(null);
+   const [isLoading, setIsLoading] = useState(true);
+ 
+   const onAuthStateChanged = async user => {
+     await setCurrentUser(user);
+     setIsLoading(false);
+   };
+ 
+   useEffect(() => {
+     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+     return subscriber;
+   }, []);
+ 
+   if (isLoading) {
+     return null;
+   }
+ 
+   return (
+     <NavigationContainer>
+       {currentUser ? <HomeScreen /> : <AuthStackNavigator />}
+     </NavigationContainer>
+   );
+ };
+ 
+ export default App;
